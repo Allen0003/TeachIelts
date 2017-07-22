b8285f438552143eaceb28887364f9c23b7bdef9
Allen0003 <apss1943@gmail.com>
2017-07-22 21:42:40 +1000

    const

---
+++ src/util/Const.java
@@ -0,0 +1,16 @@
+package util;
+
+import java.util.logging.Logger;
+
+public class Const {
+
+	public final static String systemName = "TeachIelts";
+
+	public final static String sqlDriver = "com.mysql.jdbc.Driver";
+
+	public final static String sqlUrl = "jdbc:mysql://localhost:3306";
+
+	public final static String sqlUsername = "root";
+	public final static String sqlPassword = "apss1943";
+	public static final Logger LOGGER = Logger.getLogger(systemName);
+}
